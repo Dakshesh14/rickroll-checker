791603176995e175d889d3b04d64461e6381a700
Dev-Jain04 <dakshesh.jain14@gmail.com>
2021-09-02 23:19:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+
+from time import sleep
+
+# beautifulsoup4
+# https://pypi.org/project/beautifulsoup4/
+
+from bs4 import BeautifulSoup
+
+# termcolor (for making colorfull text in the console)
+# https://pypi.org/project/termcolor/
+
+from termcolor import colored
+
+prev_views = 0
+
+def main(url):
+
+    # requests send request to the url and get all the html
+    request_data = requests.get(url)
+
+    # BeautifulSoup parser that data to html so that we can extrate all the data like views, like, etc.. in this case
+    s = BeautifulSoup(request_data.text, "html.parser")
+
+    views = s.find("meta", itemprop="interactionCount")['content'] # this find function actually finds the views
+
+    return int(views)
+
+if __name__ == "__main__":
+    print("Main function ran")
+    while True:
+        print("Checking")
+        sleep(10)
+        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+        # calling the function
+        new_view = main(url)
+
+        if new_view > prev_views:
+            prev_views = new_view
+            print(colored("Someone got rickrolled", "green"))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# This Python tells if someone got rickrolled or not by printing it on the console
+
+- To run this program make sure you have Python and Pip installed.
+- Than just download all the package by running ```pip install -r requirements.txt``` in the console.
+- Than run the program by typing ```python main.py``` in the console.
